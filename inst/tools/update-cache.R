0c3f40f6ca6b5c644db9317185eb1fa54da07600
Nan Xiao <me@nanx.me>
2025-04-25 01:23:28 -0400

    Draft update-cache.R

--- inst/tools/update-cache.R
+++ inst/tools/update-cache.R
@@ -1,16 +1,45 @@
-# Requires internet, requires specific CRAN version of pkgdown installed
-# Adaptive to each pkgdown CRAN version, from >= 2.1.0?
+#' Cache pkgdown dependencies into pkgdown.offline
+#'
+#' Cache pkgdown dependencies into pkgdown.offline for each pkgdown version
+#' (>= 2.1.0) on CRAN. Running This requires internet connection.
+#'
+#' @param version pkgdown version
+#'
+#' @examples
+#' update_cache("2.1.0")
+#' update_cache("2.1.1")
+#' update_cache("2.1.2")
+update_cache <- function(version) {
+  if (version %in% c("2.1.0", "2.1.1")) {
+    remotes::install_version("pkgdown", version)
+    pkgdown.offline::clear_cache()
 
-# pkgdown v2.1.2:
+    # Object stubbing for pkgdown::as_pkgdown()
+    pkg <- list()
+    pkg$meta$template$`math-rendering` <- "mathjax"
+    pkgdown:::external_dependencies(pkg)
 
-# remotes::install_github("r-lib/pkgdown")
-pkgdown_version <- "2.1.2"
+    pkg$meta$template$`math-rendering` <- "katex"
+    pkgdown:::external_dependencies(pkg)
 
-source_cache_dir <- tools::R_user_dir("pkgdown", which = "cache")
-fs::dir_delete(source_cache_dir)
+    copy_from_cache(version)
+  }
 
-pkgdown:::external_dependencies()
+  if (version %in% c("2.1.2")) {
+    remotes::install_github("r-lib/pkgdown@f362d53", upgrade = FALSE)
 
-target_cache_dir <- file.path("inst/cache/", pkgdown_version)
+    pkgdown.offline::clear_cache()
+    pkg <- list()
+    pkgdown:::external_dependencies(pkg)
 
-fs::dir_copy(source_cache_dir, target_cache_dir, overwrite = TRUE)
+    copy_from_cache(version)
+  }
+}
+
+#' Copy deps from cache into pkgdown.offline
+copy_from_cache <- function(version) {
+  source_cache_dir <- tools::R_user_dir("pkgdown", which = "cache")
+  target_cache_dir <- file.path("inst/cache/", version)
+  fs::dir_copy(source_cache_dir, target_cache_dir, overwrite = TRUE)
+  invisible(NULL)
+}
