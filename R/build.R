d4a7ae73cd2b879a1647ca593321ff904dbda1e3
Nan Xiao <me@nanx.me>
2025-04-25 18:22:32 -0400

    Proper monkey patch for pkgdown 2.1.2

--- R/build.R
+++ R/build.R
@@ -3,15 +3,19 @@ build_site <- function(...) {
   version <- packageVersion("pkgdown")
 
   if (version %in% c("2.1.0", "2.1.1")) options(pkgdown.internet = FALSE)
-  if (version %in% c("2.1.2")) disable_cran_link()
+  if (version %in% c("2.1.2")) {
+    # <https://github.com/r-lib/pkgdown/pull/2869>
+    return_null("cran_link")
+    return_null("pkg_timeline")
+  }
 
   pkgdown::build_site(...)
 }
 
-disable_cran_link <- function() {
-  func <- getFromNamespace("cran_link", ns = "pkgdown")
+return_null <- function(f) {
+  func <- getFromNamespace(f, ns = "pkgdown")
   code <- deparse(body(func))
   code <- "{ NULL }"
   body(func) <- parse(text = code)
-  assignInNamespace("cran_link", func, ns = "pkgdown")
+  assignInNamespace(f, func, ns = "pkgdown")
 }
